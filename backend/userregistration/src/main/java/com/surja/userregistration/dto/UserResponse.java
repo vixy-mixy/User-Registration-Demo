c8fde5303cb35aa1d0ab3ffb16b451a33d7b87a8
vixy_mixy <palsurjendu1@gmail.com>
2023-10-03 15:09:30 +0530

    Added dto layer

---
+++ backend/userregistration/src/main/java/com/surja/userregistration/dto/UserResponse.java
@@ -0,0 +1,16 @@
+package com.surja.userregistration.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+    private String name;
+    private String email;
+    private boolean success;
+}
